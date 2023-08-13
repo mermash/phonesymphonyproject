e3504d03c00822a82cb4c2464d3a88e14836ae66
merkulova <merkulova.maria.1990@gmail.com>
2023-08-13 18:39:53 +0300

    fix dump

--- docker/mysql/Dump20230813/civenty_phone_operator.sql
+++ docker/mysql/Dump20230813/civenty_phone_operator.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `civenty` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `civenty`;
 -- MySQL dump 10.13  Distrib 8.0.34, for macos13 (x86_64)
 --
 -- Host: localhost    Database: civenty
@@ -48,4 +50,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-08-12  0:51:32
+-- Dump completed on 2023-08-13 18:34:11
--- docker/mysql/script.sql
+++ docker/mysql/script.sql
@@ -5,10 +5,15 @@ JOIN `civenty`. `phone_number` ph ON ph.phone_user_id = u.id
 GROUP BY u.id
 
 -- number of phone numbers for each operator
+-- SELECT op.code AS operator_code, COUNT(ph.id) AS count_phone_numbers
+-- FROM `civenty`.`phone_number` ph
+-- JOIN `civenty`.`phone_operator` op ON op.id = ph.operator_id
+-- GROUP BY op.code
+
 SELECT op.code AS operator_code, COUNT(ph.id) AS count_phone_numbers
 FROM `civenty`.`phone_number` ph
 JOIN `civenty`.`phone_operator` op ON op.id = ph.operator_id
-GROUP BY op.code
+GROUP BY op.id
 
 -- number of phone numbers for each user
 SELECT u.name AS user_name, COUNT(ph.id) AS count_phone_numbers
@@ -23,9 +28,15 @@ GROUP BY u.id
 -- JOIN `civenty`.`phone_number` ph ON ph.phone_user_id = u.id
 -- ORDER BY ph.balance DESC LIMIT 10
 
+-- SELECT u.name AS user_name, MAX(ph.balance) AS max_balance
+-- FROM `civenty`.`user` u
+-- JOIN `civenty`.`phone_number` ph ON ph.phone_user_id = u.id
+-- GROUP BY u.name
+-- ORDER BY MAX(ph.balance) DESC LIMIT 10
+
 SELECT u.name AS user_name, MAX(ph.balance) AS max_balance
 FROM `civenty`.`user` u
 JOIN `civenty`.`phone_number` ph ON ph.phone_user_id = u.id
-GROUP BY u.name
+GROUP BY u.id
 ORDER BY MAX(ph.balance) DESC LIMIT 10
 
